82e2e31854b27a0c7e29ff5e0cf5f11d78e52bf3
Ivan Ferrera <ivanferrera@gmail.com>
2024-01-19 12:08:45 -0500

    refactoring

--- server/api/messaging/services/conversationServices.ts
+++ server/api/messaging/services/conversationServices.ts
@@ -2,9 +2,6 @@
 
 import Conversation from '../models/conversationModel'
 import User from 'server/api/messaging/models/userModel'
-import { type Socket } from 'socket.io'
-import { type MessageData } from 'server/types/messageData'
-import { Message } from '../models/messageModel'
 
 const getConversation = async (conversationId: string): Promise<Conversation | null> => {
   try {
@@ -74,59 +71,6 @@ const getOrCreateConversation = async (conversationId: string, users: string[]):
   return conversation
 }
 
-const sendDirectMessage = async (data: MessageData, socket: Socket, onlineUsers: Map<string, string>): Promise<void> => {
-  const { senderId, recipientId, conversationId, message } = data
-
-  try {
-    console.log('\x1b[34m%s\x1b[0m', 'About to create conversation. or not.')
-    const conversation = await getOrCreateConversation(conversationId, [senderId, recipientId])
-    if (conversation === null) {
-      throw new Error('Conversation not found or could not be created.')
-    }
-
-    // Save message using Mongoose
-    const newMessage = new Message({
-      conversationId: conversation.id,
-      senderId,
-      recipientId,
-      textContent: message
-      // ... add other fields as necessary ...
-    })
-
-    console.log('\x1b[34m%s\x1b[0m', 'About to save to mongo...')
-    const savedMessage = await newMessage.save()
-    console.log('\x1b[34m%s\x1b[0m', 'Message saved successfully:', savedMessage.id)
-
-    // Send message to recipient if online
-    console.log('\x1b[34m%s\x1b[0m', 'Online users:', onlineUsers)
-    console.log('Keys in onlineUsers:', [...onlineUsers.keys()].map(key => ({ key, type: typeof key })))
-    console.log('Looking for:', recipientId, ', Type:', typeof recipientId)
-
-    console.log('\x1b[34m%s\x1b[0m', 'looking for: ', recipientId)
-    const recipientSocketId = onlineUsers.get(recipientId)
-    console.log('Attempting to find socket for recipientId:', recipientId, 'Found:', recipientSocketId)
-    if (recipientSocketId !== undefined) {
-      console.log('\x1b[34m%s\x1b[0m', 'sending private message to: ', recipientSocketId)
-      socket.to(recipientSocketId).emit('private_message', {
-        senderId,
-        message
-      })
-      console.log('\x1b[34m%s\x1b[0m', 'message sent:', message)
-    } else {
-      console.log('\x1b[34m%s\x1b[0m', 'recipient is offline')
-    }
-  } catch (error) {
-    // Error handling
-    if (error instanceof Error) {
-      console.error('Error in sendDirectMessage:', error.message)
-      socket.emit('send-message-error', { error: error.message })
-    } else {
-      console.error('An unknown error occurred in sendDirectMessage:', error)
-      socket.emit('send-message-error', { error: 'An unknown error occurred' })
-    }
-  }
-}
-
 export {
-  getOrCreateConversation, getConversation, getConversations, createConversation, sendDirectMessage
+  getOrCreateConversation, getConversation, getConversations, createConversation
 }
--- server/utils/socketHandlers.ts
+++ server/utils/socketHandlers.ts
@@ -1,53 +1,38 @@
 // server/utils/socketHandlers.ts
 
 import { type Server as SocketIOServer, type Socket } from 'socket.io'
-import { sendDirectMessage } from 'server/api/messaging/services/conversationServices'
 import { type MessageData } from 'server/types/messageData'
+import { type RegisterData } from 'server/types/registerData'
+import { registerUser } from 'server/api/messaging/services/socketRegisterUser'
+import { sendDirectMessage } from 'server/api/messaging/services/socketSendDirectMessage'
+import { socketDisconnect } from 'server/api/messaging/services/socketDisconnect'
 
 const onlineUsers = new Map<string, string>() // Maps userId to socketId
 const userSockets = new Map<string, string>() // Maps socketId to userId
 
 export const socketHandlers = (io: SocketIOServer): void => {
   io.on('connection', (socket: Socket) => {
-    console.log('\x1b[34m%s\x1b[0m', 'A user connected', socket.id)
+    console.log('\n\x1b[34m%s\x1b[0m', 'User connected with socket ID: ', socket.id, '\n')
 
-    socket.on('disconnect', () => {
-      console.log('\x1b[34m%s\x1b[0m', 'User disconnected', socket.id)
-      const userId = userSockets.get(socket.id) // Get the userId associated with the disconnected socket
-      if (userId !== undefined) {
-        onlineUsers.delete(userId) // Delete the user from onlineUsers using the userId
-        userSockets.delete(socket.id) // Delete the socketId from userSockets
-        console.log('\x1b[34m%s\x1b[0m', 'online users: ', onlineUsers)
-      } else {
-        console.log('\x1b[34m%s\x1b[0m', 'User not found in userSockets', socket.id)
-      }
-    })
-
-    // Define an interface for the expected data structure
-    interface RegisterData {
-      userId: string
-    }
-
-    socket.on('register', (data: RegisterData) => {
-      const userId = data.userId // No need to cast to string as it's already expected to be a string
-      console.log('Registering user:', userId)
-      const existingSocketId = onlineUsers.get(userId)
-
-      if (existingSocketId !== undefined && existingSocketId !== socket.id) {
-        userSockets.delete(existingSocketId) // Remove old socket ID mapping
-      }
-
-      console.log('\x1b[34m%s\x1b[0m', `User with ID ${userId} added or updated`)
-      onlineUsers.set(userId, socket.id) // Update or add new user with current socket ID
-      userSockets.set(socket.id, userId) // Store or update the reverse mapping
-
-      console.log('\x1b[34m%s\x1b[0m', 'online users: ', onlineUsers)
+    socket.on('register', async (data: RegisterData) => {
+      await registerUser(data, socket, onlineUsers, userSockets)
     })
 
     socket.on('private_message', async (messageData: MessageData): Promise<void> => {
-      console.log('\x1b[34m%s\x1b[0m', '\nMessage received')
-      // Send the message to the recipient if they are online
       await sendDirectMessage(messageData, socket, onlineUsers)
+    })
+
+    interface MessageReadData {
+      string: string
+    }
+
+    socket.on('messageDelivered', async (messageReadData: MessageReadData): Promise<void> => {
+      console.log('Message delivered: ', messageReadData)
+      // update message status in mongo
+    })
+
+    socket.on('disconnect', async () => {
+      await socketDisconnect(socket, onlineUsers, userSockets)
     })
   })
 }
