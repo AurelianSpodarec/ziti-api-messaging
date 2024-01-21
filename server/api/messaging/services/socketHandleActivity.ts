d6d8d9ec798fc613d967ddaff32b894f01136ea1
Ivan Ferrera <ivanferrera@gmail.com>
2024-01-20 19:30:28 -0500

    initial commit

---
+++ server/api/messaging/services/socketHandleActivity.ts
@@ -0,0 +1,29 @@
+import { type Server as SocketIOServer, type Socket } from 'socket.io'
+
+export const handleActivity = async (
+  data: any,
+  socket: Socket,
+  onlineUsers: Map<string, string>,
+  io: SocketIOServer
+): Promise<void> => {
+  try {
+    const { userId, recipientId } = data // Assuming data contains recipientId
+    const recipientSocketId = onlineUsers.get(recipientId as string)
+    if (recipientSocketId !== undefined) {
+      io.to(recipientSocketId).emit('activity', userId)
+      console.log('activity', userId, 'to', recipientId)
+    } else {
+      console.log(`Recipient (${recipientId}) is not online.`)
+    }
+  } catch (error) {
+    // Error handling
+    if (error instanceof Error) {
+      console.error('Error in handleActivity:', error.message)
+      socket.emit('handle-activoty-error', { error: error.message })
+    } else {
+      console.error('An unknown error occurred in handleActivity:', error)
+      socket.emit('handle-activity-error', { error: 'An unknown error occurred' })
+    }
+  }
+  console.log('\n')
+}
