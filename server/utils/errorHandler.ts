39bd5b62477bc075d4d0417e26ef988bd593dcbe
Ivan Ferrera <ivanferrera@gmail.com>
2024-01-23 09:30:30 -0500

    using errorhandling middleware instead

--- server/utils/errorHandler.ts
+++
@@ -1,7 +0,0 @@
-// server/utils/errorHandler.ts
-import { type Response } from 'express'
-
-export function handleError (res: Response, error: Error, message: string, statusCode: number = 500): void {
-  console.error(error.message)
-  res.status(statusCode).json({ error: message })
-}
